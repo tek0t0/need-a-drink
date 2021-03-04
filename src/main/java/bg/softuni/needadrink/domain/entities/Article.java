1071485064c12e246fecd38ef87b160dde5c82b9
Stoyan Mitev <tekoto@abv.bg>
2021-03-04 22:41:14 +0200

    Index page created.

--- src/main/java/bg/softuni/needadrink/domain/entities/Article.java
+++ src/main/java/bg/softuni/needadrink/domain/entities/Article.java
@@ -8,21 +8,23 @@ import java.time.LocalDateTime;
 @Entity
 @Table(name = "articles")
 public class Article extends BaseEntity{
-    private String name;
+    private String title;
     private UserEntity author;
     private int liked;
     private String coverImgUrl;
     private LocalDateTime addedOn;
+    private String description;
+    private String content;
 
     public Article() {
     }
 
-    public String getName() {
-        return name;
+    public String getTitle() {
+        return title;
     }
 
-    public Article setName(String name) {
-        this.name = name;
+    public Article setTitle(String name) {
+        this.title = name;
         return this;
     }
 
@@ -60,6 +62,24 @@ public class Article extends BaseEntity{
 
     public Article setAddedOn(LocalDateTime addedOn) {
         this.addedOn = addedOn;
+        return this;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Article setDescription(String description) {
+        this.description = description;
+        return this;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public Article setContent(String content) {
+        this.content = content;
         return this;
     }
 }
