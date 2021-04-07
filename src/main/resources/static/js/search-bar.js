643d85b6c5c374e3222e42ca8c7038e77477985f
Stoyan Mitev <tekoto@abv.bg>
2021-04-07 11:28:24 +0300

    Search functionality finished.

--- src/main/resources/static/js/search-bar.js
+++ src/main/resources/static/js/search-bar.js
@@ -14,8 +14,9 @@ console.log(allCocktails);
 searchBar.addEventListener('keyup', (e) => {
     const searchingCharacters = searchBar.value.toLowerCase();
     let filteredCocktails = allCocktails.filter(cocktail => {
-        return cocktail.name.toLowerCase().includes(searchingCharacters);
-        // || cocktail. .artist.toLowerCase().includes(searchingCharacters);
+        console.log(cocktail);
+        return cocktail.name.toLowerCase().includes(searchingCharacters)
+            || cocktail.description.toLowerCase().includes(searchingCharacters);
     });
     displayCocktails(filteredCocktails);
 })
@@ -24,23 +25,18 @@ searchBar.addEventListener('keyup', (e) => {
 const displayCocktails = (cocktail) => {
     cocktailsList.innerHTML = cocktail
         .map((c) => {
-            return `  <div sec:authorize="isAuthenticated()" class="mx-auto mt-5 " id="cocktail_of_the_day_container">
-            <h2 class="text-center text-white my-3">Cocktail of the day</h2>
-            <div th:object="${cocktailSearchViewModel}" class="d-flex" id="cocktail-box">
+            return `  <div class="mx-auto mt-5 " id="cocktail_of_the_day_container">
+            <h2 class="text-center text-white my-3" hidden>Cocktail of the day</h2>
+            <div  class="d-flex" id="cocktail-box">
 
-                <img class="h-25" style="" th:src="*{imgUrl}" id="cocktail_details_img" alt="cocktail image">
+                <img class="h-25" style="" src="${c.imgUrl}" id="cocktail_details_img" alt="cocktail image"
+                data-holder-rendered="true">
 
                 <div class="flex-column p-2">
-                    <div><h3 th:text="*{name}" class="my-4"></h3></div>
-                    <div><p th:text="*{description}"></p></div>
-                    <div>
-                        <ul>
-                            <li th:each="i: ${cocktailSearchViewModel.getIngredientsNames()}"  th:text="${i}"
-                                style="list-style: circle inside;"></li>
-                        </ul>
-                    </div>
+                    <div><h3  class="my-4">${c.name}</h3></div>
+                    <div><p >${c.description}</p></div>
                     <div class="d-flex justify-content-around my-3">
-                        <a th:href="@{/cocktails/details/{id}(id=*{id})}" class="btn btn-dark">Details</a>
+                        <a href="/cocktails/details/${c.id}" class="btn btn-dark">Details</a>
                     </div>
                 </div>
             </div>
